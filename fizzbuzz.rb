e7e576e1f1e289229e1142adc591bcbe707429e6
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-10 18:50:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 15 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif int % 3 == 0
     "Buzz"
-  elsif int % 15 == 0
+  elsif int % 5 == 0
     "FizzBuzz"
   else
     nil
